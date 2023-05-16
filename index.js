2e616963d114fbe315f762abafc1c89a228698d8
James Okolie <zebedee788@gmail.com>
2023-05-17 00:32:05 +0400

    Required tools added

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = epress()
+const port = 8000
+
+let movies = [
+    {
+        id: '1',
+        title: 'Inception',
+        director: 'Christopher Nolan',
+        release_date: '2010-07-16'
+    },
+    {
+        id: '2',
+        title: 'The Iriahman',
+        director: 'Martin Scorsein',
+        release_date: '2010-07-16'
+    },
+]
